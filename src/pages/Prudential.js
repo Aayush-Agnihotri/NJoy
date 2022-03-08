54c57b75b4736d81c8645361a65459ec72406140
mdasilva01 <78115070+mdasilva01@users.noreply.github.com>
2022-03-08 14:29:06 -0500

    Add files via upload

---
+++ src/pages/Prudential.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Template from "./Template"
+
+const Prudential = () => {
+  return (
+    <>
+        <Template
+        name={"Prudential Center"}
+        place_num={5}
+        stars={4}
+        mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12093.17653729907!2d-74.1797992111059!3d40.733552930562865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c253829a70f1d3%3A0x4d3efa6b5652a5ac!2sPrudential%20Center!5e0!3m2!1sen!2sus!4v1646341324708!5m2!1sen!2sus"}
+        pnumb={"973-757-6000"}
+        address={"25 Lafayette St, Newark, NJ 07102"}
+        hours={"Mon to Fri 11am to 6pm, Closed on Saturday & Sunday (with the exception of any event, and the box office will open at 11am)"}
+        website={"https://prucenter.com/"}
+        faq1q={"How much does parking cost?"}
+        faq1a={"Event parking can range from $30 to $40."}
+        faq2q={"Can I bring a camera to events?"}
+        faq2a={"The Prudential Center has no rules against cameras, but it depends on the event."}
+        faq3q={"Is it accessible for the disabled?"}
+        faq3a={"There are multiple ramps, disabled parking options, and elevators to assist those with disabilities."}
+        h1={"GRAMMY Museum Experience in the Prudential Center"}
+        h2={"Hell's Kitchen Lounge"}
+        h3={"Newark Museum"}
+        about={"Prudential Center is a multi-purpose indoor arena in the central business district of Newark, New Jersey, United States. It was designed by HOK Sport (now Populous), with the exterior designed by Morris Adjmi Architects. Opened in 2007, it is the home of the New Jersey Devils of the National Hockey League (NHL) and the men's basketball program of Seton Hall University, known as the Seton Hall Pirates. The arena officially seats 16,514 patrons for hockey games and up to 18,711 for basketball. Fans and sports writers have affectionately nicknamed the arena \"The Rock\" in reference to the Rock of Gibraltar, the corporate logo of Prudential Financial, a financial institution that owns the naming rights to the arena and is headquartered within walking distance of it."}
+        thingstoknow={"Besides hosting sporting events for the New Jersey Devils or Seton Hall University, the Prudential Center holds a plethora of other types of events. Other sporting events, such as for wrestling, gymnastics, and boxing, have been hosted by the center. Additionally, concerts take place in the facility often, with some past musicians including Bruce Springsteen, Bon Jovi, and The Eagles."}
+        covid={"As a result of the Executive Order from the City of Newark on March 2, effective immediately and unless required otherwise by the artist or promoter of a particular show, guests will no longer be required to show proof of vaccination or proof of a negative test to enter Prudential Center and will not be required to wear face masks to attend events. However, anyone who wishes to do so can still wear a mask."}
+        addInfo={"Bags are not permitted inside of Prudential Center. The only exceptions are soft sided clutches/purses/wallets under 4” x 7” with a shoulder strap or wrist strap attached, and medical or childcare bags under 12” x 16” x 6”. Patrons with exceptions should enter at the Ford Tower or the Lafayette Street entrance and expect to have their bags X-rayed. Bags that do not meet these expectations must be returned to the guest’s vehicle or stored in a Prudential Center supplied locker at the guest’s expense."} />
+    </>
+  )
+}
+
+export default Prudential
---
+++ src/pages/Seaside.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Template from "./Template"
+
+const Seaside = () => {
+  return (
+    <>
+        <Template
+        name={"Seaside Heights Boardwalk"}
+        place_num={1}
+        stars={4.5}
+        mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12236.11720886298!2d-74.07999931150339!3d39.94073422424438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c19a31b88eff63%3A0xc72d1cdf6d7351c2!2sSeaside%20Boardwalk!5e0!3m2!1sen!2sus!4v1646341127090!5m2!1sen!2sus"}
+        pnumb={"732-830-3700"}
+        address={"Boardwalk, Seaside Heights, NJ 08751"}
+        hours={"Open 24 Hours Mon-Sun"}
+        website={"https://exit82.com/boardwalk/"}
+        faq1q={"When are the rides operating?"}
+        faq1a={"FAQ #1 answer"}
+        faq2q={"How much does it cost to get on the beach?"}
+        faq2a={"FAQ #2 answer"}
+        faq3q={"Is anything open year-round?"}
+        faq3a={"FAQ #3 answer"}
+        h1={"Casino Pier"}
+        h2={"The Jersey Shore House"}
+        h3={"Breakwater Beach Waterpark"}
+        about={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        thingstoknow={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        covid={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        addInfo={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
+    </>
+  )
+}
+
+export default Seaside
