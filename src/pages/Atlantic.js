a48063bdc56a15d1f91c66c45ecb672f1dd9b523
sriramammu22 <81487822+sriramammu22@users.noreply.github.com>
2022-03-08 14:27:12 -0500

    Add files via upload

---
+++ src/pages/Atlantic.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Template from "./Template"
+
+const Atlantic = () => {
+  return (
+    <>
+        <Template name={"Atlantic City"} 
+        place_num={3} 
+
+        stars={5} 
+
+        mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d197381.89253333746!2d-74.5925653778518!3d39.3767927338931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c0dd576e5cc721%3A0x4a6fcb43e9675262!2sAtlantic%20City%2C%20NJ!5e0!3m2!1sen!2sus!4v1646341247927!5m2!1sen!2sus"} 
+        pnumb={"1-888-AC-VISIT"} 
+
+        address={""} 
+
+        hours={"Sun	10AM–5PM Mon 10AM–5PM Tues 10AM–5PM Wed 10AM–5PM Thurs 10AM–5PM Fri 10AM–12PM Sat 10AM–5PM"}
+         website={"https://www.atlanticcitynj.com/"}
+
+          faq1q={"When was the first casino built in Atlantic City?"}
+          
+           faq1a={"May 26, 1978"} 
+           faq2q={"Why is Atlantic City so famous?"} 
+           faq2a={"The city grew rapidly and offered lodging, dining, entertainment and amusements for all ages, tastes and incomes."} 
+           faq3q={"Is Atlantic City like Las Vegas?"} 
+           faq3a={"Las Vegas and Atlantic City are two major tourist destinations known for gambling and nightlife, yet they're 2,500 miles apart."} 
+           h1={"Steel Pier"} 
+           h2={"Atlantic City Aquarium"} 
+           h3={"Ripley's Believe It or Not!"} 
+           about={"Atlantic City is a city close to the east coast of the Atlantic Ocean at New Jersey. It was founded during May 1, 1854. There are so many casinos in Atlantic City. This city is known for casinos, boardwalk, and beaches."} 
+           thingstoknow={"1. The parking lots are expensive  2. Kids are not allowed to be at the casinos."} 
+           covid={""} 
+           addInfo={"During September 2020, Tripadvisor named Atlantic City as one of the top 10 travel destinations in the USA."} 
+           />
+    </>
+   )
+  }
+
+export default Atlantic;
