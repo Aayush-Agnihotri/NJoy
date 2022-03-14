d3fb078f0cd7770d5c361a73b52bded5f8484d81
Aayush <aayush.agni1@gmail.com>
2022-03-14 10:15:21 -0400

    commit

--- src/pages/Pnc.js
+++ src/pages/Pnc.js
@@ -7,26 +7,25 @@ const Pnc = () => {
         <Template
         name={"PNC Bank Art Center"}
         place_num={4}
-        stars={4}
+        stars={4.5}
         mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12154.930450149815!2d-74.18558011127774!3d40.39261841420427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c233f026f4a2b1%3A0xa8c3a6448a270276!2sPNC%20Bank%20Arts%20Center!5e0!3m2!1sen!2sus!4v1646341296300!5m2!1sen!2sus"}
-        pnumb={"(123) 456-7890"}
-        address={"145 Plainfield Ave, Metuchen, NJ 08840"}
-        hours={"Sun	10AM–5PM Mon 10AM–5PM Tues 10AM–5PM Wed 10AM–5PM Thurs 10AM–5PM Fri 10AM–12PM Sat 10AM–5PM"}
-        website={"https://stjoes.org/"}
-        faq1q={"FAQ #1"}
-        faq1a={"FAQ #1 answer"}
-        faq2q={"FAQ #2"}
-        faq2a={"FAQ #2 answer"}
-        faq3q={"FAQ #3"}
-        faq3a={"FAQ #3 answer"}
-        h1={"Highlight 1"}
-        h2={"Highlight 2"}
-        h3={"Highlight 3"}
-        about={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        thingstoknow={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        covid={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        addInfo={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
-    </>
+        pnumb={"(732) 203-2500"}
+        address={"116 Garden State Pkwy, Holmdel, NJ 07733"}
+        hours={"12pm-9pm on days of performance only"}
+        website={"http://banksarts centre.com/"}
+        faq1q={"Is Parking Free at the PNC Bank Arts Center?"}
+        faq1a={"Parking is not free at the PNC Bank Arts Center, all event attendees must pay a charge of $6, this includes those who don’t possess a car, as it includes a per-ticket parking fee scheme. Larger vehicles such as those carrying 3 people are just at $18 per car."}
+        faq2q={"Can I bring food into the PNC Bank Arts Center?"}
+        faq2a={"Events attendees are permitted to bring their own food, however it must be stored and sealed inside of a clear plastic bag, container."}
+        faq3q={"Can I buy tickets on the door?"}
+        faq3a={"There is never a set answer, you can try, but please note, you will be getting all the left over tickets which may not have the best display. For the most enjoyable evening, we recommend buying tickets in advance."}
+        h1={"PNC Bank Arts Center features one of the most extravagant drive through light displays in the tri – state area. The spectacular is a 2.5-mile (4.0 km) drive through a forest of lights. Over 5 million lights were used over 225 displays."}
+        h2={"This year the PNC Bank Arts Center has some of the biggest music artists in the world, from pop legend Lionel Richie, to the best live bands Dave Matthews band and the Zac Brown Band, to 70’s rock legends Journey and Boston – the Band. It’s all kicking off here in New Jersey, don’t miss out, head over to our schedule page for listings of the latest events! OR Check out our Pnc Bank Arts Center Rules"}
+        h3={"The Venue has had some of the biggest music artists ever! These include James Taylor, Bruce Springsteen, Ringo Starr and His All-Starr Band, Jackson Browne, and Whitney Houston."}
+        about={"Voted in the TOP 5 theatres in America, the PNC Bank Arts Amphitheatre is a premier venue, with a mega seating capacity of 17,500, with the grass areas allowing for a whopping 10,500 people! The Amphitheatre was originally named Garden State Arts Center. The amphitheatre was designed by noted modernist architect Edward Durell Stone and featured open sides covered by a 200-foot (61 m), saucer-like roof supported by eight large concrete pillars."}
+        thingstoknow={"The Light Spectacular usually runs from the end of November through January 2. You will not see anything like it in the surrounding area and is worth checking out, even if you are not attending a show! This year the PNC Bank Arts Center has some of the biggest music artists in the world, from pop legend Lionel Richie, to the best live bands Dave Matthews band and the Zac Brown Band, to 70’s rock legends Journey and Boston – the Band. It’s all kicking off here in New Jersey, don’t miss out, head over to our schedule page for listings of the latest events! OR Check out our Pnc Bank Arts Center Rules!"}
+        covid={"We are continuing to monitor the development of the COVID-19 pandemic and working hard to minimize its impact on our customers, partners, and employees. Our preparations are focused on the need to ensure the health of our community and the continuity of our business activities. While we all love live events and experiences, we remain mindful of the general health of our community and especially individuals in high-risk groups. This communication is designed to answer your questions about what happens if your live event is canceled or postponed."}
+        addInfo={"The Garden State Arts Center first opened on June 12, 1968, with a program featuring pianist Van Cliburn, conductor Eugene Ormandy, and the Philadelphia Orchestra."} />\</>
   )
 }
 
