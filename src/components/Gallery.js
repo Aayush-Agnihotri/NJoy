70478418fa93364c82a12943c29b7d7301c2dd45
Aayush <aayush.agni1@gmail.com>
2022-03-14 08:55:20 -0400

    commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,90 +1,92 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import "./Gallery.css";
+import { Link } from 'react-router-dom';
 
 const Gallery = () => {
   return (
     <>
         <div className='header'>Attractions</div>
         <br></br>
+
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
-            <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 1</div>
+          <Link to='/seaside'>
+            <img src={require('../images/Seaside/SeasideGallery1.jpeg')} />
+            <div className='title'>Seaside Heights Boardwalk</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/six'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 2</div>
+            <div className='title'>Six Flags</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
-        </div>
-        
-        <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
-            <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 3</div>
-            <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/atlantic'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 4</div>
+            <div className='title'>Atlantic City</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/Pnc'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 5</div>
+            <div className='title'>PNC Bank Art Center</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
-        </div>
-        
-        <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
-            <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 6</div>
-            <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/Prudential'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 7</div>
+            <div className='title'>Prudential Center</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/park'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 8</div>
+            <div className='title'>Liberty State Park</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/liberty'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 9</div>
+            <div className='title'>Liberty Science Center</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
         </div>
 
         <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
+          <Link to='/gardens'>
             <img src={require('../images/home/forest.jpg')} />
-            <div className='title'>Place 10</div>
+            <div className='title'>Botanical Gardens</div>
             <div className="desc">Add a description of the image here</div>
-          </a>
+          </Link>
+        </div>
+
+        <div className="gallery">
+          <Link to='/mall'>
+            <img src={require('../images/home/forest.jpg')} />
+            <div className='title'>American Dream Mall</div>
+            <div className="desc">Add a description of the image here</div>
+          </Link>
+        </div>
+
+        <div className="gallery">
+          <Link to='/princeton'>
+            <img src={require('../images/home/forest.jpg')} />
+            <div className='title'>Princeton</div>
+            <div className="desc">Add a description of the image here</div>
+          </Link>
         </div>
 
     </>
