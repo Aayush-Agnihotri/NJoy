1956b24e1ba5f631dfe6775d479001ee114a51c0
rmodhera <78107787+rmodhera@users.noreply.github.com>
2022-03-15 14:52:18 -0400

    Update GardensData.ts

--- src/components/Reviews/GardensData.ts
+++ src/components/Reviews/GardensData.ts
@@ -3,7 +3,7 @@ export const GardensData = [
       authorUrl: '',
       avatarUrl:
         'https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc',
-      createdAt: new Date(1654488000000),
+      createdAt: new Date(1645765200000),
       fullName: 'Henry Meditz ✔',
       text: 'The Botanical Gardens are awesome! I absolutely loved it here and enjoyed going here with my family.',
     },
@@ -11,7 +11,7 @@ export const GardensData = [
       authorUrl: '',
       avatarUrl:
         'https://media.vanityfair.com/photos/5c0d9391ea5c1607c3a6dd07/9:16/w_621,h_1104,c_limit/t-Ben-Shapiro.jpg',
-      createdAt: new Date(1655265600000),
+      createdAt: new Date(1645765200000),
       fullName: 'Ben Shapiro',
       text: 'This place sucks! I hated the flowers!',
     },
@@ -19,7 +19,7 @@ export const GardensData = [
       authorUrl: '',
       avatarUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
-      createdAt: new Date(1659499200000),
+      createdAt: new Date(1647147600000),
       fullName: 'Harenda Greg ✔',
       text: 'Very beautiful park and good place to hangout with family. It has limited parking.',
     },
