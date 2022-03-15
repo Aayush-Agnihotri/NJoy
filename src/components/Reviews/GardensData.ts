ed245018ccd3805d7ab1548f5fea83102b283baa
rmodhera <78107787+rmodhera@users.noreply.github.com>
2022-03-15 14:42:19 -0400

    Update GardensData.ts

--- src/components/Reviews/GardensData.ts
+++ src/components/Reviews/GardensData.ts
@@ -3,24 +3,24 @@ export const GardensData = [
       authorUrl: '',
       avatarUrl:
         'https://isobarscience.com/wp-content/uploads/2020/09/default-profile-picture1.jpg',
-      createdAt: new Date(1640297561680),
-      fullName: 'Noah Johnson ✔',
-      text: 'Gardens',
+      createdAt: new Date(1660297561680),
+      fullName: 'Henry Meditz ✔',
+      text: 'The Botanical Gardens are awesome! I absolutely loved it here and enjoyed going here with my family.',
     },
     {
       authorUrl: '',
       avatarUrl:
         'https://campussafetyconference.com/wp-content/uploads/2020/08/iStock-476085198.jpg',
-      createdAt: new Date(1641197561680),
-      fullName: 'Liam Anderson',
-      text: 'This place sucks! I tripped and fell on the boardwalk.',
+      createdAt: new Date(1671197561680),
+      fullName: 'Noah Weinberg',
+      text: 'This place sucks! I hated the flowers!',
     },
     {
       authorUrl: '',
       avatarUrl:
         'https://previews.123rf.com/images/meysye/meysye1904/meysye190400002/121250278-female-silhoutte-avatar-default-avatar-profile-picture-photo-placeholder.jpg',
-      createdAt: new Date(1642197561680),
-      fullName: 'Olivia Smith ✔',
-      text: 'Absolutely love this place!',
+      createdAt: new Date(167797561680),
+      fullName: 'Harenda Greg ✔',
+      text: 'Very Beautiful Park and good place to hangout with Family. It has limited parking.',
     },
   ];
