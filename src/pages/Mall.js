9d3550a00b7d827e7bba8d824bbbd8e1e3f2601c
Srikrishna2022 <78455665+Srikrishna2022@users.noreply.github.com>
2022-03-15 13:22:54 -0500

    Update Mall.js

--- src/pages/Mall.js
+++ src/pages/Mall.js
@@ -14,17 +14,17 @@ const Mall = () => {
         website={"https://www.american dream.com/"} 
         faq1q={"What is the Lucky Seven Offer and when does it close?"} 
         faq1a={"The Lucky Seven Offer gives you the opportunity to experience seven amazing attractions in one week for $250. This offer closes on April 30."} 
-        faq2q={"Where should you visit first for the best shopping and dining experience?"} 
-        faq2a={"The Avenue"} 
+        faq2q={"What is coming soon to American Dream Mall"} 
+        faq2a={"The Escape Room, Blast 7D, and New Jersey Hall of Fame"} 
         faq3q={"When is Pauly D's Amusement Park Takeover?"} 
         faq3a={"April 1 and 2"} 
-        h1={"Nickelodeon Universe is home to the Western Hemisphere's Biggest Indoor Theme Park!"} 
-        h2={"Instagram Moments are areas throughout the mall designed specifically for social media photography."} 
-        h3={"Big Snow is North America's first and only indoor, real-snow, year-round ski and snow resort."} 
-        about={""} 
-        thingstoknow={""} 
-        covid={""} 
-        addInfo={""} />
+        h1={"Nickelodeon Universe"} 
+        h2={"The Avenue"} 
+        h3={"Big Snow"} 
+        about={"American Dream, developed by Triple Five Group, owners of the two largest retail and entertainment centers in North America – Mall of America and West Edmonton Mall, will provide customers with an entirely unique experience. The property offers a curated mix of unparalleled entertainment, retail and dining opportunities, all under one roof. American Dream is approximately 3 million square feet and projects 40 million visitors annually. The complex includes over 450 shops, services and amenities, complemented by the best in entertainment, food, art and culture. American Dream will also feature a distinct shopping environment dedicated to iconic luxury brands and younger, fashion-forward retail."} 
+        thingstoknow={"1. Some shop times may vary. Please view directory to plan your visit. 2. American Dream is surrounded by an extensive network of highways and arterial roadways. The entire complex, including MetLife Stadium, Meadowlands Racetrack, and American Dream facilities is accessible from I-95, Route 120 and Route 3. 3. You should purchase tickets in advance for parks such as DreamWorks Water Mark and Nickoldean Universe."} 
+        covid={"1. Frequent hand washing and use of hand sanitizers is encouraged throughout American Dream. Hand sanitizers are provided throughout the property and both guests and DreamMakers are reminded to frequently wash their hands with soap and water for at least 20 seconds or use hand sanitizer. 2. The frequency of cleaning and sanitizing has been increased throughout American Dream, with emphasis on frequent contact surfaces using cleaning products and protocols which meet the United States Environmental Protection Agency (EPA) guidelines and Center for Disease Control (CDC) guidelines. 3. Face Coverings are no longer required for fully vaccinated guests, however, are recommended while visiting American Dream. Guests that are not fully vaccinated should continue to wear a face covering and practice physical distancing (remain 6 feet apart) throughout your experience at American Dream."} 
+        addInfo={"Facts about Highlights: 1. Nickelodeon Universe is home to the Western Hemisphere's Biggest Indoor Theme Park! 2. Instagram Moments are areas throughout the mall designed specifically for social media photography. 3. Big Snow is North America's first and only indoor, real-snow, year-round ski and snow resort.} />
     </>
   )
 }
