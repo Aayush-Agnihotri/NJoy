c0cb922236341337f460ef210dc93a635e237347
mdasilva01 <78115070+mdasilva01@users.noreply.github.com>
2022-03-15 14:54:55 -0400

    Update PrudentialData.ts

--- src/components/Reviews/PrudentialData.ts
+++ src/components/Reviews/PrudentialData.ts
@@ -3,7 +3,7 @@ export const PrudentialData = [
       authorUrl: '',
       avatarUrl:
         'https://mir-s3-cdn-cf.behance.net/project_modules/1400/2d686878402297.5ca6085aa8e1d.jpg',
-      createdAt: new Date(164029720000),
+      createdAt: new Date(1500000000000),
       fullName: 'Jamie Would ✔',
       text: 'One time I saw a Devils game here... good time.',
     },
@@ -11,7 +11,7 @@ export const PrudentialData = [
       authorUrl: '',
       avatarUrl:
         'https://live-production.wcms.abc-cdn.net.au/eaebae2a6ed46cc8f5cc89a0df3a36f8?impolicy=wcms_crop_resize&cropH=1388&cropW=1046&xPos=874&yPos=557&width=862&height=1149',
-      createdAt: new Date(100000000000),
+      createdAt: new Date(1000000000000),
       fullName: 'Donald Trobama',
       text: 'The bathrooms were spick and span and finding parking was relatively easy. I seek to revisit shortly for the furry convention.',
     },
@@ -19,7 +19,7 @@ export const PrudentialData = [
       authorUrl: '',
       avatarUrl:
         'https://upload.wikimedia.org/wikipedia/commons/f/fb/Anthro_vixen_colored.jpg',
-      createdAt: new Date(100006000000),
+      createdAt: new Date(1000060000000),
       fullName: 'Georgia Bridgers',
       text: 'The furry convention was beyond my expectations!',
     },
