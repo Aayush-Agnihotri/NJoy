0cb3e1941bd73cc041530ec5b3f84e30119e4ea1
Aayush <aayush.agni1@gmail.com>
2022-03-17 13:01:25 -0400

    commit

--- src/components/Reviews/ParkData.ts
+++ src/components/Reviews/ParkData.ts
@@ -2,25 +2,25 @@ export const ParkData = [
     {
       authorUrl: '',
       avatarUrl:
-        'https://isobarscience.com/wp-content/uploads/2020/09/default-profile-picture1.jpg',
+        'https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       createdAt: new Date(1640297561680),
-      fullName: 'Noah Johnson ✔',
-      text: 'Park',
+      fullName: 'Emanuel Tucker',
+      text: 'Easily accessible with tons of parking and long walking trails by the water',
     },
     {
       authorUrl: '',
       avatarUrl:
-        'https://campussafetyconference.com/wp-content/uploads/2020/08/iStock-476085198.jpg',
+        'https://www.postplanner.com/hs-fs/hub/513577/file-2886416984-png/blog-files/facebook-profile-pic-vs-cover-photo-sq.png?width=250&height=250&name=facebook-profile-pic-vs-cover-photo-sq.png',
       createdAt: new Date(1641197561680),
-      fullName: 'Liam Anderson',
-      text: 'This place sucks! I tripped and fell on the boardwalk.',
+      fullName: 'Russell Hawes',
+      text: 'Beautiful place for a walk, run, skate or push your baby around!',
     },
     {
       authorUrl: '',
       avatarUrl:
-        'https://previews.123rf.com/images/meysye/meysye1904/meysye190400002/121250278-female-silhoutte-avatar-default-avatar-profile-picture-photo-placeholder.jpg',
+        'https://wallpapercave.com/wp/wp5756323.jpg',
       createdAt: new Date(1642197561680),
-      fullName: 'Olivia Smith ✔',
-      text: 'Absolutely love this place!',
+      fullName: 'Dana Nixon ✔',
+      text: 'It’s a great place to spend some time!',
     },
   ];
