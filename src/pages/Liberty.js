d6c4353b6c7e34cdd0974065ba64d5280eae586e
Aayush <aayush.agni1@gmail.com>
2022-03-17 12:53:00 -0400

    commit

--- src/pages/Liberty.js
+++ src/pages/Liberty.js
@@ -10,7 +10,7 @@ const Liberty = () => {
          mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6100.417424579071!2d-74.44459175325719!3d40.13763441718942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c1649b50339cd7%3A0xeed622849d816ad4!2sSix%20Flags%20Great%20Adventure-%20Theme%20Park%2C%20Jackson%20Township%2C%20NJ%2008514!5e0!3m2!1sen!2sus!4v1645311538111!5m2!1sen!2sus"} 
          pnumb={"(201) 200-1000"} 
          address={"222 Jersey City Blvd, Jersey City, NJ 07305"} 
-         hours={"Sun	10AM–5PM Mon CLOSED Tues CLOSED Wed CLOSED Thurs 10AM–4PM Fri 10AM–4PM Sat 10AM–5PM"} 
+         hours={"Sun 10AM–5PM Thurs 10AM–4PM Fri 10AM–10PM Sat 10AM–5PM"} 
          website={"https://lsc.org/"} 
          faq1q={"How much does parking cost?"} faq1a={"The parking lot is owned by the state of New Jersey and operated by a private company. The fee for cars and vans during the day is $7.00."} 
          faq2q={"Are food and drinks allowed inside?"} faq2a={"Guests can purchase sandwiches, salads, pizza, chicken, drinks, snacks, and more in our cafe on the second floor. Outside food and beverages are not permitted."} 
@@ -21,7 +21,7 @@ const Liberty = () => {
          about={"Liberty Science Center is a learning and informaational center that strives to allow guests to expand on their scientific knowledge. The Science Center houses 12 museum exhibition halls, a live animal collection with 110 species, giant aquariums, a 3D theater, live simulcast surgeries, hurricane- and tornado-force wind simulators, K-12 classrooms and labs, teacher-development programs, and the Western Hemisphere's biggest planetarium—the Jennifer Chalsty Planetarium and LSC Giant Dome Theater. More than 750,000 students, teachers, and parents visit Liberty Science Center each year, and tens of thousands more participate in the Center's off-site and online programs."} 
          thingstoknow={"Liberty Science Center's mission is to inspire the next generation of scientists and engineers and excite learners of all ages about the power, promise, and pure fun of science and technology. with new exhibits coming every year, with informational knowledge to share."} 
          covid={"We’re limiting total capacity to help guests maintain distance. We ask that you please purchase tickets online in advance, as many of our time slots and theater shows sell out fast. For everyone's safety, especially those too young to be vaccinated, guests must properly wear an approved mask. This policy applies to all guests ages 2 and over, regardless of vaccination status. Masks must fully cover the nose and mouth and secure under the chin."} 
-         addInfo={"We require proof of full COVID-19 vaccination for all visitors age 12 years and older. We also require a valid photo ID for all visitors age 18 years and older. "} />
+         addInfo={"We require proof of full COVID-19 vaccination for all visitors age 12 years and older. We also require a valid photo ID for all visitors age 18 years and older."} />
     </>
   )
 }
