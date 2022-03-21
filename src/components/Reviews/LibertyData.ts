0bae297436651c814faa9c400868d4ea5e4dfdf0
MJH22 <100873436+MJH22@users.noreply.github.com>
2022-03-21 13:36:48 -0400

    Update LibertyData.ts

--- src/components/Reviews/LibertyData.ts
+++ src/components/Reviews/LibertyData.ts
@@ -2,16 +2,16 @@ export const LibertyData = [
     {
       authorUrl: '',
       avatarUrl:
-        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vibrant-red-and-yellow-gazania-flower-royalty-free-image-594915222-1554736685.jpg',
+        'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1592721086l/54199957._SX318_.jpg',
         
       createdAt: new Date(1640297561680),
-      fullName: 'Noah Johnson ✔',
+      fullName: 'Nolan ✔',
       text: 'Liberty',
     },
     {
       authorUrl: '',
       avatarUrl:
-        'https://campussafetyconference.com/wp-content/uploads/2020/08/iStock-476085198.jpg',
+        'https://comicvine.gamespot.com/a/uploads/scale_medium/12/124259/8102817-moonknight.jpg',
       createdAt: new Date(1641197561680),
       fullName: 'Liam Anderson',
       text: 'This place sucks! I tripped and fell on the boardwalk.',
@@ -19,7 +19,7 @@ export const LibertyData = [
     {
       authorUrl: '',
       avatarUrl:
-        'https://previews.123rf.com/images/meysye/meysye1904/meysye190400002/121250278-female-silhoutte-avatar-default-avatar-profile-picture-photo-placeholder.jpg',
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vibrant-red-and-yellow-gazania-flower-royalty-free-image-594915222-1554736685.jpg',
       createdAt: new Date(1642197561680),
       fullName: 'Olivia Smith ✔',
       text: 'Absolutely love this place!',
